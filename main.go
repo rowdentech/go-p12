2358723b22d7884137c24d2f64e145e9e1664e6e
Doug Fitzmaurice <dougfitzmaurice@rowdentech.com>
2020-07-29 12:38:44 +0100

    Add support for creating trust stores (with no private key)

--- main.go
+++ main.go
@@ -31,27 +31,17 @@ var caPaths arrayFlag
 
 func main() {
 
-	certPathPtr := flag.String("cert", "", "Path to the primary certificate")
-	keyPathPtr := flag.String("key", "", "Path to the private key for the primary certificate")
+	certPathPtr := flag.String("cert", "", "(Optional) Path to the primary certificate")
+	keyPathPtr := flag.String("key", "", "(Optional) Path to the private key for the primary certificate")
 	p12PathPtr := flag.String("p12", "", "Output path for the P12 you wish to create")
 	flag.Var(&caPaths, "ca", "(Optional) Path to a CA certificate to append to the p12 (can be passed multiple times)")
 
 	flag.Parse()
 
-	if *certPathPtr == "" || *keyPathPtr == "" || *p12PathPtr == "" {
-		fmt.Println("go-p12 helps create P12 keystore files for use with Firefox, Windows, Java applications")
-		fmt.Println("Usage: go-p12 -p12 <file.p12> -cert <cert.crt> -key <key.pem> -ca <root.crt>")
-		flag.PrintDefaults()
-		os.Exit(1)
-	}
-
-	x509Cert, err := decodeCertificate(*certPathPtr)
-	if err != nil {
-		log.Fatalf("failed to decode certificate: " + err.Error())
-	}
-	x509Key, err := decodePrivateKey(*keyPathPtr)
-	if err != nil {
-		log.Fatalf("failed to decode private key: " + err.Error())
+	hasCAs := len(caPaths) > 0
+	hasKeyAndCert := *certPathPtr != "" && *keyPathPtr != ""
+	if *p12PathPtr == "" || (!hasCAs && !hasKeyAndCert) {
+		printUsageAndExit()
 	}
 
 	x509CAs := []*x509.Certificate{}
@@ -64,15 +54,54 @@ func main() {
 		x509CAs = append(x509CAs, cert)
 	}
 
-	fmt.Printf("Please enter the new password for the P12 file: ")
-	password, _ := terminal.ReadPassword(int(os.Stdin.Fd()))
+	var pfxData []byte
+	var err error
 
-	pfxData, err := pkcs12.Encode(rand.Reader, x509Key, x509Cert, x509CAs, string(password))
-	if err != nil {
-		log.Fatalf("failed to encode p12 data: " + err.Error())
+	if hasKeyAndCert {
+		//Creating an "identity store" - a certificate and matching private key, and possibly a number of CA certificates for the chain
+		x509Cert, err := decodeCertificate(*certPathPtr)
+		if err != nil {
+			log.Fatalf("failed to decode certificate: " + err.Error())
+		}
+		x509Key, err := decodePrivateKey(*keyPathPtr)
+		if err != nil {
+			log.Fatalf("failed to decode private key: " + err.Error())
+		}
+
+		password := promptForPassword()
+
+		pfxData, err = pkcs12.Encode(rand.Reader, x509Key, x509Cert, x509CAs, password)
+		if err != nil {
+			log.Fatalf("failed to encode p12 data: " + err.Error())
+		}
+	} else {
+		//Creating a "trust" store - a list of CA certificates
+		password := promptForPassword()
+
+		pfxData, err = pkcs12.EncodeTrustStore(rand.Reader, x509CAs, password)
+		if err != nil {
+			log.Fatalf("failed to encode p12 data: " + err.Error())
+		}
 	}
 
 	err = ioutil.WriteFile(*p12PathPtr, pfxData, 0644)
+	if err != nil {
+		log.Fatalf("failed to write p12 data: " + err.Error())
+	}
+}
+
+func printUsageAndExit() {
+	fmt.Println("go-p12 helps create P12 keystore files for use with Firefox, Windows, Java applications\n")
+	fmt.Println("Identity store with a private key: go-p12 -p12 <file.p12> -cert <cert.crt> -key <key.pem> -ca <root.crt>")
+	fmt.Println("Trust store with only CA certs:    go-p12 -p12 <file.p12> -ca <root.crt> -ca <intermediate.crt>\n")
+	flag.PrintDefaults()
+	os.Exit(1)
+}
+
+func promptForPassword() string {
+	fmt.Printf("Please enter a new password for the P12 file: ")
+	password, _ := terminal.ReadPassword(int(os.Stdin.Fd()))
+	return string(password)
 }
 
 func decodePrivateKey(path string) (crypto.PrivateKey, error) {
